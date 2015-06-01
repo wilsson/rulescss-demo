58ea9e84535815b8d0a562da9ea343870b0244fb
wilsson <wilsonft83@gmail.com>
2015-05-31 22:20:51 -0500

    feat(gulpfile):agregando gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp        = require('gulp'),
+	jade        = require('gulp-jade'),
+	stylus      = require('gulp-stylus'),
+	watch       = require('gulp-watch'),
+	browserSync = require('browser-sync');
+
+/*****************************************/
+var path = {
+	jadeTask   : 'myApp/jade/**/*.jade',
+	stylusTask : 'myApp/stylus/views/**/*.styl'
+};
+
+var pathDest = {
+	html : 'src/',
+	css  : 'src/css/'
+};
+
+/*****************************************/
+gulp.task('html',function(){
+	gulp.src(path.jadeTask)
+		.pipe(jade({
+			pretty:true
+		}))
+		.pipe(gulp.dest(pathDest.html));
+});
+
+gulp.task('stylus',function(){
+	gulp.src(path.stylusTask)
+		.pipe(stylus())
+		.pipe(gulp.dest(pathDest.css));
+});
+
+/*****************************************/
+gulp.task("browser-sync", function(){
+    browserSync({
+        server: {
+            baseDir:'src/',
+            directory: true
+        },
+        port: 3000
+    });
+});
+
+/*****************************************/
+var reload = browserSync.reload; 
+var jadeTask   = ['html',reload];
+var stylusTask = ['stylus',reload];
+
+/*****************************************/
+gulp.task('watch',function(){
+	gulp.watch(path.jadeTask,jadeTask);
+	gulp.watch(path.stylusTask,stylusTask);
+});
+
+/*****************************************/
+gulp.task("server", ["browser-sync", "watch"]);
